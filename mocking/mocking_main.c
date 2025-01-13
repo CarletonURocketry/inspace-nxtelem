7cde5f8709c194831cdab56873d2cc25c39c6eb2
Angus Jull <angusjull1@gmail.com>
2025-01-13 17:23:18 -0500

    fix build

--- mocking/mocking_main.c
+++ mocking/mocking_main.c
@@ -9,23 +9,23 @@
 
 #include "data_romfs.h"
 
-#define CONFIG_MOCK_ROMFS_DEVNO 1
-#define CONFIG_MOCK_ROMFS_SECTORSIZE 512
+#define CONFIG_MOCKING_ROMFS_DEVNO 10
+#define CONFIG_MOCKING_ROMFS_SECTORSIZE 512
 
 
 #define STR_RAMDEVNO(m)    #m
 #define MKMOUNT_DEVNAME(m) "/dev/ram" STR_RAMDEVNO(m)
-#define NSECTORS(b)        (((b)+CONFIG_MOCK_ROMFS_SECTORSIZE-1)/CONFIG_MOCK_ROMFS_SECTORSIZE)
+#define NSECTORS(b)        (((b)+CONFIG_MOCKING_ROMFS_SECTORSIZE-1)/CONFIG_MOCKING_ROMFS_SECTORSIZE)
 
-#define MOUNT_DEVNAME MKMOUNT_DEVNAME(CONFIG_MOCK_ROMFS_DEVNO)
+#define MOUNT_DEVNAME MKMOUNT_DEVNAME(CONFIG_MOCKING_ROMFS_DEVNO)
 
 int mount_mock_fs(void) {
     int ret;
     struct boardioc_romdisk_s desc;
 
-    desc.minor    = CONFIG_MOCK_ROMFS_DEVNO;         /* Minor device number of the ROM disk. */
+    desc.minor    = CONFIG_MOCKING_ROMFS_DEVNO;         /* Minor device number of the ROM disk. */
     desc.nsectors = NSECTORS(data_img_len);              /* The number of sectors in the ROM disk */
-    desc.sectsize = CONFIG_MOCK_ROMFS_SECTORSIZE;       /* The size of one sector in bytes */
+    desc.sectsize = CONFIG_MOCKING_ROMFS_SECTORSIZE;       /* The size of one sector in bytes */
     desc.image    = (FAR uint8_t *)data_img;             /* File system image */
 
     ret = boardctl(BOARDIOC_ROMDISK, (uintptr_t)&desc);
@@ -35,8 +35,8 @@ int mount_mock_fs(void) {
     }
 
     printf("Mounting ROMFS filesystem at target=%s with source=%s\n",
-            CONFIG_INSPACE_MOCK_MOUNT, MOUNT_DEVNAME);
-    ret = mount(MOUNT_DEVNAME, CONFIG_INSPACE_MOCK_MOUNT, "romfs",
+            CONFIG_INSPACE_MOCKING_MOUNT, MOUNT_DEVNAME);
+    ret = mount(MOUNT_DEVNAME, CONFIG_INSPACE_MOCKING_MOUNT, "romfs",
                 MS_RDONLY, NULL);
     if (ret < 0) {
         printf("ERROR: Mount failed: %s\n", strerror(errno));
