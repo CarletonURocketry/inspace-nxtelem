609b926915b525f157ae3e78bd76c7c404585619
Angus Jull <angusjull1@gmail.com>
2025-01-13 16:57:18 -0500

    rearrange configuration
    * moved fakesensor config to boards in nuttx
    * renamed config keys
    * added dependencies for mocking program
    * used code from romfs example for generating romfs on build
    * updated romfs dev mount style to be like romfs example

--- mocking/mocking_main.c
+++ mocking/mocking_main.c
@@ -1,46 +1,50 @@
 #include <nuttx/config.h>
 
-#include <nuttx/sensors/fakesensor.h>
-#include <nuttx/sensors/sensor.h>
-#include <nuttx/drivers/ramdisk.h>
-
 #include <sys/mount.h>
+#include <sys/boardctl.h>
 #include <errno.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
-#include "data.img.h"
+#include "data_romfs.h"
 
 #define CONFIG_MOCK_ROMFS_DEVNO 1
-#define CONFIG_MOCK_ROMFS_MOUNTPOINT "/usr/local/share"
 #define CONFIG_MOCK_ROMFS_SECTORSIZE 512
 
 
-#define CONFIG_MOCK
 #define STR_RAMDEVNO(m)    #m
 #define MKMOUNT_DEVNAME(m) "/dev/ram" STR_RAMDEVNO(m)
 #define NSECTORS(b)        (((b)+CONFIG_MOCK_ROMFS_SECTORSIZE-1)/CONFIG_MOCK_ROMFS_SECTORSIZE)
 
 #define MOUNT_DEVNAME MKMOUNT_DEVNAME(CONFIG_MOCK_ROMFS_DEVNO)
-#define MOCK_BARO_FILE CONFIG_MOCK_ROMFS_MOUNTPOINT "baro.csv"
 
-int mount_mock_fs() {
-    // Created a ROMDISK using genromfs, then converted it into a header file earlier
+int mount_mock_fs(void) {
     int ret;
-    ret = romdisk_register(CONFIG_MOCK_ROMFS_DEVNO, data_img, NSECTORS(data_img_len), CONFIG_MOCK_ROMFS_SECTORSIZE);
+    struct boardioc_romdisk_s desc;
+
+    desc.minor    = CONFIG_MOCK_ROMFS_DEVNO;         /* Minor device number of the ROM disk. */
+    desc.nsectors = NSECTORS(data_img_len);              /* The number of sectors in the ROM disk */
+    desc.sectsize = CONFIG_MOCK_ROMFS_SECTORSIZE;       /* The size of one sector in bytes */
+    desc.image    = (FAR uint8_t *)data_img;             /* File system image */
+
+    ret = boardctl(BOARDIOC_ROMDISK, (uintptr_t)&desc);
     if (ret < 0) {
         printf("ERROR: Failed to create RAM disk: %s\n", strerror(errno));
-        return -1;
+        return 1;
     }
-    printf("Mounting ROMFS filesystem at %s with source /dev/ram1\n",
-        CONFIG_MOCK_ROMFS_MOUNTPOINT);
-    ret = mount(MOUNT_DEVNAME, CONFIG_MOCK_ROMFS_MOUNTPOINT, "romfs", MS_RDONLY, NULL);
+
+    printf("Mounting ROMFS filesystem at target=%s with source=%s\n",
+            CONFIG_INSPACE_MOCK_MOUNT, MOUNT_DEVNAME);
+    ret = mount(MOUNT_DEVNAME, CONFIG_INSPACE_MOCK_MOUNT, "romfs",
+                MS_RDONLY, NULL);
     if (ret < 0) {
-        printf("ERROR: Failed to mount: %s", strerror(errno));
-        return -1;
+        printf("ERROR: Mount failed: %s\n", strerror(errno));
+        return 1;
     }
+    return 0;
 }
 
-int main(int argc, FAR char *argv[]) {
-    mount_mock_fs();
+int main(int argc, char **argv) {
+    return mount_mock_fs();
 }
