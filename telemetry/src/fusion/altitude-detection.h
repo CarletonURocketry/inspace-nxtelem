a2d3556baaeb37cbd268c03b919f668a6a3c5d35
Angus Jull <angusjull1@gmail.com>
2025-04-17 18:44:47 -0400

    start liftoff detection

---
+++ telemetry/src/fusion/altitude-detection.h
@@ -0,0 +1,13 @@
+// set up detection
+
+// give a new sample
+
+// tell me when the next sample should be given
+
+// check if we've launched
+
+// check if we're landed
+
+// check if we're idle
+
+
