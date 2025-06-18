36a0420b9b291f5850c2e18214532d12f05826cf
Angus Jull <angusjull1@gmail.com>
2025-06-17 23:28:56 -0400

    Swap out detection algorithms
    Hopefully improved and simplified the detection alrogirthms by adopting
    a plain median filter and average filter on altitude and acceleration.
    
    By using acceleration as well as altitude, and by ditching the linear
    regression, we can improve the accuracy of detections a lot

--- telemetry/src/fusion/detector.c
+++ telemetry/src/fusion/detector.c
@@ -5,44 +5,147 @@
 
 #include "detector.h"
 
-#define DETECTION_DEBUG
+/* The that measurements should be valid for */
+#define STALE_MEASUREMENT_TIME 1000000 /* 1 second in microseconds */
 
-#if defined(DETECTION_DEBUG)
-#include <stdio.h>
-#define DEBUG_PRINT(...) printf(__VA_ARGS__)
-#else
-#define DEBUG_PRINT(...)
-#endif
+/* A measurement hasn't been updated in a long time and shouldn't be used */
+#define IS_STALE(last_update, now) \
+  (now - last_update > STALE_MEASUREMENT_TIME)
 
-struct detection_criteria {
-  float max_vel;        /* Max abs rate of change of altitude for this event in m/s */
-  float min_vel;        /* Min abs rate of change of altitude for this event in m/s */
-  float min_altitude;   /* The minimum altitude this event can be detected at in m*/
-  float max_altitude;   /* The maximum altitude this event can be detected at in m*/
-};
+/* The maximum amount the altitude can change by in meters to consider us landed */
+#define LANDED_ALT_WINDOW_SIZE 10.0f
 
-const struct detection_criteria airborne_thresholds = {
-  .min_vel = 25.0,
-  .max_vel = FLT_MAX,
-  .min_altitude = -FLT_MAX,
-  .max_altitude = FLT_MAX
-};
+/* The time in seconds that altitude variation must be within LANDED_ALT_WINDOW_SIZE */
+#define LANDED_ALT_WINDOW_DURATION 5.0f
 
-const struct detection_criteria landed_thresholds = {
-  .min_vel = -FLT_MAX,
-  .max_vel = 0.5,
-  .min_altitude = -FLT_MAX,
-  .max_altitude = 100.0
-};
+/* The change in altitude when idle to enter the airborne state */
+#define LIFTOFF_ALT_THRESHOLD 20.0f
 
-void detector_init(struct detector *detector) {
-  return;
+/* The acceleration above which we consider the rocket to be flying */
+#define LIFTOFF_ACCEL_THRESHOLD 15.0f
+
+void median_filter_init(struct median_filter *filter, float *sorted, float *time_ordered, int size) {
+  filter->size = 0;
+  filter->sorted = sorted;
+  circ_buffer_init(&filter->time_ordered, time_ordered, size, sizeof(float));
 }
 
-static int criteria_met(struct detection_criteria *criteria, struct rocket_dynamics *dynamics) {
-  int test = dynamics->velocity > criteria->min_vel && dynamics->velocity < criteria->max_vel;
-  test &= dynamics->altitude > criteria->min_altitude && dynamics->altitude < criteria->max_altitude;
-  return test;
+static void remove_from_sorted(float to_remove, float *sorted, int size) {
+  int remove_index = 0;
+  for (; remove_index < size; remove_index++) {
+    if (sorted[remove_index] == to_remove) {
+      break;
+    }
+  }
+  // Remove the oldest value by shifting to the left
+  for (; remove_index < size - 1; remove_index++) {
+    sorted[remove_index] = sorted[remove_index + 1];
+  }
+}
+
+static void insert_into_sorted(float value, float *sorted, int size) {
+  // Find position to insert the new value
+  int insert_index = 0;
+  for (; insert_index < size; insert_index++) {
+    if (sorted[insert_index] > value) {
+      break;
+    }
+  }
+  // Shift elements to the right after the insertion point
+  for (int i = size; i >= insert_index; i--) {
+    sorted[i + 1] = sorted[i];
+  }
+  sorted[insert_index] = value;
+}
+
+float median_filter_add(struct median_filter *filter, float new_value) {
+  float to_remove = 0.0f;
+  if (circ_buffer_push_out(&filter->time_ordered, &new_value, &to_remove)) {
+    remove_from_sorted(to_remove, filter->sorted, filter->size);
+    filter->size--;
+  }
+  insert_into_sorted(new_value, filter->sorted, filter->size);
+  filter->size++;
+  // Return the median (assume odd size)
+  return filter->sorted[filter->size / 2];
+}
+
+void average_filter_init(struct average_filter *filter, float *buffer, int size) {
+  circ_buffer_init(&filter->buffer, buffer, size, sizeof(float));
+  filter->sum = 0.0f;
+}
+
+float average_filter_add(struct average_filter *filter, float new_value) {
+  float old_value = 0.0f;
+  if (circ_buffer_push_out(&filter->buffer, &new_value, &old_value)) {
+    filter->sum -= old_value;
+  }
+  filter->sum += new_value;
+  return filter->sum / circ_buffer_size(&filter->buffer);
+}
+
+void detector_init(struct detector *detector) {
+  detector->abs_max_altitude = -FLT_MAX;
+  detector->alt_window_max = -FLT_MAX;
+  detector->alt_window_min = FLT_MAX;
+  detector->alt_window_duration = 0.0f;
+  detector->landed_alt = 0.0f; // Figure out how to set properly later
+
+  detector->current_time = 0;
+  detector->last_alt_update = 0;
+  detector->last_accel_update = 0;
+  detector->current_alt = 0.0f;
+  detector->current_accel = 0.0f;
+
+  median_filter_init(&detector->alts.median, detector->alts.median_backing_sorted, detector->alts.median_backing_time_ordered, ALTITUDE_MEDIAN_FILTER_SIZE);
+  average_filter_init(&detector->alts.average, detector->alts.average_backing, ALTITUDE_AVERAGE_FILTER_SIZE);
+
+  median_filter_init(&detector->accels.median, detector->accels.median_backing_sorted, detector->accels.median_backing_time_ordered, ACCEL_MEDIAN_FILTER_SIZE);
+  average_filter_init(&detector->accels.average, detector->accels.average_backing, ACCEL_AVERAGE_FILTER_SIZE);
+}
+
+void detector_add_alt(struct detector *detector, struct altitude_sample *sample) {
+  float median = median_filter_add(&detector->alts.median, sample->altitude);
+  detector->current_alt = average_filter_add(&detector->alts.average, sample->altitude);
+
+  if (detector->current_alt > detector->abs_max_altitude) {
+    detector->abs_max_altitude = detector->current_alt;
+  }
+
+  // Check for landed state here
+  if (detector->alt_window_max - detector->alt_window_min > LANDED_ALT_WINDOW_SIZE) {
+    // If the window is too large, reset it
+    detector->alt_window_max = detector->current_alt;
+    detector->alt_window_min = detector->current_alt;
+    detector->alt_window_duration = 0;
+  }
+  else if (detector->current_alt > detector->alt_window_max) {
+    detector->alt_window_max = detector->current_alt;
+    detector->alt_window_duration = 0;
+  }
+  else if (detector->current_alt < detector->alt_window_min) {
+    detector->alt_window_min = detector->current_alt;
+    detector->alt_window_duration = 0;
+  }
+  else {
+    detector->alt_window_duration += sample->time - detector->last_alt_update;
+  }
+
+  detector->last_alt_update = sample->time;
+}
+
+float detector_get_alt(struct detector *detector) {
+  return detector->current_alt;
+}
+
+void detector_add_accel(struct detector *detector, struct accel_sample *sample) {
+  float median = median_filter_add(&detector->accels.median, sample->acceleration);
+  detector->current_accel = average_filter_add(&detector->accels.average, sample->acceleration);
+  detector->last_accel_update = sample->time;
+}
+
+float detector_get_accel(struct detector *detector) {
+  return detector->current_accel;
 }
 
 /**
@@ -52,21 +155,28 @@ static int criteria_met(struct detection_criteria *criteria, struct rocket_dynam
  * @param dynamics The current dynamics of the rocket, including altitude and velocity
  * @return The detected event, or DETECTOR_NO_EVENT if none was detected
  */
-enum detector_event detector_detect(struct detector *detector, enum flight_state_e state, struct rocket_dynamics *dynamics) {
-  // Don't use data that has not been updated at all
-  if (dynamics->last_update == 0) {
-    return DETECTOR_NO_EVENT;
-  }
-  if (state == STATE_IDLE) {
-    if (criteria_met(&airborne_thresholds, dynamics)) {
-      DEBUG_PRINT("Detected airborne event at altitude %f m and velocity %f m/s\n", dynamics->altitude, dynamics->velocity);
-      return DETECTOR_AIRBORNE_EVENT;
+enum detector_event detector_detect(struct detector *detector, enum flight_state_e state) {
+  switch (state) {
+    case STATE_IDLE:
+      if (!IS_STALE(detector->last_alt_update, detector->current_time)
+          && detector->current_alt > (detector->landed_alt + LIFTOFF_ALT_THRESHOLD)) {
+        return DETECTOR_AIRBORNE_EVENT;
+      }
+      else if (!IS_STALE(detector->last_accel_update, detector->current_time)
+               && fabsf(detector->current_accel) > LIFTOFF_ACCEL_THRESHOLD) {
+        return DETECTOR_AIRBORNE_EVENT;
+      }
+      break;
+
+    case STATE_AIRBORNE: {
+      break;
     }
-  } else if (state == STATE_AIRBORNE) {
-    if (criteria_met(&landed_thresholds, dynamics)) {
-      DEBUG_PRINT("Detected landing event at altitude %f m and velocity %f m/s\n", dynamics->altitude, dynamics->velocity);
-      return DETECTOR_LANDING_EVENT;
+
+    case STATE_LANDED: {
+      break;
     }
+
+    default:
+      return DETECTOR_NO_EVENT;
   }
-  return DETECTOR_NO_EVENT;
 }
--- telemetry/src/fusion/detector.h
+++ telemetry/src/fusion/detector.h
@@ -1,20 +1,91 @@
 #ifndef _DETECTOR_H_
 #define _DETECTOR_H_
 
-#include "filter.h"
+#include "circular-buffer.h"
 #include "../rocket-state/rocket-state.h"
+
+#define ALTITUDE_MEDIAN_FILTER_SIZE 5
+#define ALTITUDE_AVERAGE_FILTER_SIZE 10
+
+#define ACCEL_MEDIAN_FILTER_SIZE 5
+#define ACCEL_AVERAGE_FILTER_SIZE 10
+
 
 /* Detection events */
 enum detector_event {
   DETECTOR_NO_EVENT,       /* No event has occured */
   DETECTOR_AIRBORNE_EVENT, /* The rocket is airborne */
+  DETECTOR_APOGEE_EVENT,   /* The rocket has reached its maximum height */
   DETECTOR_LANDING_EVENT   /* The rocket is landed*/
 };
 
+struct altitude_sample {
+  float altitude; /* The altitude in meters */
+  float time;     /* The time in seconds since mission start */
+};
+
+struct median_filter {
+  int size;
+  float *sorted;
+  struct circ_buffer time_ordered;
+};
+
+struct average_filter {
+  struct circ_buffer buffer; /* Circular buffer */
+  float sum;
+};
+
+struct alt_filter {
+  struct altitude_sample median_backing_sorted[ALTITUDE_MEDIAN_FILTER_SIZE];
+  struct altitude_sample median_backing_time_ordered[ALTITUDE_MEDIAN_FILTER_SIZE];
+  struct altitude_sample average_backing[ALTITUDE_AVERAGE_FILTER_SIZE];
+  struct median_filter median;   /* Median filter for altitude */
+  struct average_filter average; /* Average filter for altitude */
+
+};
+
+struct accel_sample {
+  float acceleration; /* The acceleration in m/s^2 */
+  float time;         /* The time in seconds since mission start */
+};
+
+struct accel_filter {
+  struct accel_sample median_backing_sorted[ACCEL_MEDIAN_FILTER_SIZE];
+  struct accel_sample median_backing_time_ordered[ACCEL_MEDIAN_FILTER_SIZE];
+  struct accel_sample average_backing[ACCEL_AVERAGE_FILTER_SIZE];
+  struct median_filter median;   /* Median filter for acceleration */
+  struct average_filter average; /* Average filter for acceleration */
+};
+
 /* Information related to detecting liftoff or landing */
-struct detector {};
+struct detector {
+  struct alt_filter alts;     /* Filtering for altitude data */
+  struct accel_filter accels; /* Filtering for acceleration data */
+
+  uint64_t current_time;
+  uint64_t last_alt_update;
+  uint64_t last_accel_update;
+  float current_alt;
+  float current_accel;
+  
+  float landed_alt;           /* The altitude at landing or a sensible default */
+  float abs_max_altitude;     /* The maximum altitude ever recorded */
+  float alt_window_max;       /* The maximum altitude recently recorded */
+  float alt_window_min;       /* The minimum altitude recently recorded */
+  float alt_window_duration;  /* The minimum of the timestamps of recent_min/max_alt */
+};
 
 void detector_init(struct detector *detector);
-enum detector_event detector_detect(struct detector *detector, enum flight_state_e state, struct rocket_dynamics *dynamics); 
+void detector_add_alt(struct detector *detector, struct altitude_sample *sample);
+float detector_get_alt(struct detector *detector);
+void detector_add_accel(struct detector *detector, struct accel_sample *sample);
+float detector_get_accel(struct detector *detector);
+enum detector_event detector_detect(struct detector *detector, enum flight_state_e state);
+
+void median_filter_init(struct median_filter *filter, float *sorted, float *time_ordered, int size);
+float median_filter_add(struct median_filter *filter, float new_value);
+
+void average_filter_init(struct average_filter *filter, float *buffer, int size);
+float average_filter_add(struct average_filter *filter, float new_value);
 
 #endif /* _DETECTOR_H_ */
