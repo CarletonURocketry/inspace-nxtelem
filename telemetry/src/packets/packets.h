7528d345b5fd3f2d3d1a92edab9ec7033129aaa6
Elias Hawa <elias.hawa2468@gmail.com>
2025-08-13 20:42:19 -0400

    Fix voltage packet to match packet spec

--- telemetry/src/packets/packets.h
+++ telemetry/src/packets/packets.h
@@ -167,10 +167,10 @@ void coord_blk_init(struct coord_blk_t *b, const int32_t lat, const int32_t lon)
 struct volt_blk_t {
     /* The offset from the absolute time in the header in milliseconds */
     int16_t time_offset;
-    /* Unique sensor ID. */
-    uint16_t id;
     /* Voltage in millivolts. */
     int16_t voltage;
+    /* Unique sensor ID. */
+    uint8_t id;
 } TIGHTLY_PACKED;
 
 void volt_blk_init(struct volt_blk_t *b, const uint16_t id, const int16_t voltage);
